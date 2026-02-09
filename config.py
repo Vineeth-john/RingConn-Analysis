6b84474ee0ff964fc0b0acbed261ff695296d338
Vineeth <vineethjohn77@yahoo.com>
2026-02-09 10:08:43 -0500

    02_notebook_added

---
+++ config.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+PROJECT_ROOT = Path("/Users/vineeth/Desktop/RingCon/RingConn-Analysis")
+DATA_RAW = PROJECT_ROOT / "data" / "raw"
+DATA_PROCESSED = PROJECT_ROOT / "data" / "processed"
